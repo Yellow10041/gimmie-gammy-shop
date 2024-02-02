6f925baab7c29ad8a2272c1c4049d76e2e10b68d
Yellow10041 <yellowvatral@gmail.com>
2024-02-02 22:01:11 +0200

    fix

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = process.env.PUBLIC_SERVER_URL
-export const API_URL = SERVER_URL + "/api"
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+export const API_URL = SERVER_URL + "/api";
