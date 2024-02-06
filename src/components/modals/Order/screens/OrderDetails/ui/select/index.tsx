3d2f408f3704cb23cb049916a1f97b6f8539afe1
Yellow10041 <yellowvatral@gmail.com>
2024-02-06 19:34:39 +0200

    fix

--- src/components/modals/Order/screens/OrderDetails/ui/select/index.tsx
+++ src/components/modals/Order/screens/OrderDetails/ui/select/index.tsx
@@ -1,14 +1,7 @@
 import { useClickAway } from "@uidotdev/usehooks";
 import clsx from "clsx";
 import gsap from "gsap";
-import {
-  FC,
-  MutableRefObject,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import { FC, MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
@@ -25,7 +18,7 @@ interface ISelect {
 
 export const Select: FC<ISelect> = ({ items, onDataChange, clearTrigger }) => {
   const [searchValue, setSearchValue] = useState<string>("");
-  const [selectValue, setSelectValue] = useState<string>(items[0].label);
+  const [selectValue, setSelectValue] = useState<string>("");
 
   const itemsSearch = useMemo(() => {
     return items.filter((item) => item.label.includes(searchValue));
@@ -97,24 +90,15 @@ export const Select: FC<ISelect> = ({ items, onDataChange, clearTrigger }) => {
   }, [clearTrigger]);
 
   return (
-    <div
-      className={clsx(styles.Select)}
-      ref={(ref) => (refClick.current = ref as Element)}
-    >
+    <div className={clsx(styles.Select)} ref={(ref) => (refClick.current = ref as Element)}>
       <div className={clsx(styles.Select_header)} onClick={handleOpen}>
         <div className={clsx(styles.Select_header_title)}>
           {/* <div className={clsx(styles.Select_header_title_icon)}>
             <IconBed />
           </div> */}
-          <input
-            className={clsx(styles.Select_header_title_text)}
-            onChange={(e) => setSearchValue(e.target.value)}
-            ref={refInput}
-          />
+          <input className={clsx(styles.Select_header_title_text)} placeholder="Choose your country" onChange={(e) => setSearchValue(e.target.value)} ref={refInput} autoComplete="off" />
         </div>
-        <div
-          className={clsx(styles.Select_header_icon, isOpen && styles.active)}
-        >
+        <div className={clsx(styles.Select_header_icon, isOpen && styles.active)}>
           <IconArrowSmall />
         </div>
       </div>
@@ -126,10 +110,7 @@ export const Select: FC<ISelect> = ({ items, onDataChange, clearTrigger }) => {
               <div className={clsx(styles.Select_menu_inner)} ref={refSelect}>
                 {itemsSearch.map((e, i) => (
                   <div
-                    className={clsx(
-                      styles.Select_menu_item,
-                      indexActiveElem === i && styles.active
-                    )}
+                    className={clsx(styles.Select_menu_item, indexActiveElem === i && styles.active)}
                     onClick={() => {
                       handleOpen();
                       setIndexActiveElem(i);
